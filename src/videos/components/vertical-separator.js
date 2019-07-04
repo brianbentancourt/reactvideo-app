cbce9f7378cd7e7083a18d52ad852f78f924ac0d
brianbentancourt <brianbentancourt9@gmail.com>
2019-07-03 21:54:21 -0300

    working with list

---
+++ src/videos/components/vertical-separator.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+    View,
+    Text,
+    StyleSheet
+} from 'react-native'
+
+function VerticalSeparator(props){
+    return(
+        <View style={[
+            styles.separator,
+            {
+                borderTopColor: (props.color) ? props.color : '#eaeaea'
+            }
+            ]}>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    separator:{
+        borderTopWidth: 1
+    },
+    text:{
+        fontSize: 16
+    }
+})
+
+export default VerticalSeparator
--- src/videos/containers/suggestions-list.js
+++ src/videos/containers/suggestions-list.js
@@ -3,8 +3,16 @@ import {
     FlatList,
     Text
 } from 'react-native'
+import Layout from '../components/suggestions-list-layout'
+import Empty from '../components/empty'
+import VerticalSeparator from '../components/vertical-separator'
 
 class SuggestionList extends Component {
+
+    renderEmpty = () => <Empty text='No hay sugerencias :(' />
+
+    ItemSeparator = () => <VerticalSeparator />
+
     render() {
         const list = [
             {
@@ -18,7 +26,17 @@ class SuggestionList extends Component {
         ]
 
         return (
-            <FlatList data={list} renderItem={({item}) =><Text>{item.title}</Text> } />
+            <Layout
+                title='Recomendado para ti'
+            >
+                <FlatList
+                    data={list}
+                    ListEmptyComponent={this.renderEmpty}
+                    ItemSeparatorComponent={this.ItemSeparator}
+                    renderItem={({ item }) => <Text>{item.title}</Text>}
+                />
+            </Layout>
+
         )
     }
 }
